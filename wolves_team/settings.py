77f3d5e13ef949d4100659c514421fb4a80fa189
basemtaha53-coder <basemtaha53@gmail.com>
2026-05-28 21:34:44 +0300

    add vercel config

--- wolves_team/settings.py
+++ wolves_team/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 import os
 import dj_database_url
+ALLOWED_HOSTS = ['.vercel.app']
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
